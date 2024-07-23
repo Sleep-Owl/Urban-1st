c2ba2cae728d69a295be4f8c4f5a0bd1f56cf9a9
Sleep-Owl <moon41@yandex.ru>
2024-07-23 18:03:32 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,17 @@
+# Словари
+my_dict = {'Alex': 1990, 'Maria': 1996}
+print('Dict:', my_dict)
+print('Existing value:', my_dict['Alex'])
+print('Not existing value:', my_dict.get('All'))
+my_dict.update({'Natasha': 1989, 'Andrey': 1990})
+Name = my_dict.pop('Maria')
+print('Deleted value:', Name)
+print('Modified dictionary:', my_dict)
+
+# Множества
+my_set = {1, 1, 'Яблоко', 'Яблоко', 42.314, True, 42.314}
+print(my_set)
+my_set.add(13)
+my_set.add((14, 5))
+my_set.discard(42.314)
+print(my_set)
