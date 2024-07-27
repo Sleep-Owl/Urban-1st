736a420923717c717798544dc4e99462997e0b01
Sleep-Owl <moon41@yandex.ru>
2024-07-27 13:25:12 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,15 @@
+first = 123
+second = 456
+third = 789
+
+if first==second or first==third or second==first:
+    print(3)
+else: print(0)
+
+first = 42
+second = 69
+third = 42
+
+if first==second or first==third or second==third:
+    print(2)
+else: print(0)
