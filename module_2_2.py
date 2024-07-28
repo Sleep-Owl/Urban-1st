62358c4105a6837ff4135e84adc2379a66cf2d21
Sleep-Owl <moon41@yandex.ru>
2024-07-28 14:56:47 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,17 +1,6 @@
-first = 123
-second = 456
-third = 789
-
-if first == second and second == third:
-    print(3)
-elif first == second or second == third or first == third:
-    print(2)
-else:
-    print(0)
-
-first = 42
-second = 69
-third = 42
+first = int(input("Введите первое целое число: "))
+second = int(input("Введите второе целое число: "))
+third = int(input("Введите третье целое число: "))
 
 if first == second and second == third:
     print(3)
