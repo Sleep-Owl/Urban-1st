0813eed99a533e307bf7c490c5cd96e0a727d7a0
Aleksey <moon41@yandex.ru>
2024-08-07 17:03:29 +0300

    Рекурсия + цикл, сравнение

--- module_3_5.py
+++ module_3_5.py
@@ -1,3 +1,4 @@
+# Выполнение задачи с помощью рекурсии
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
@@ -8,3 +9,18 @@ def get_multiplied_digits(number):
 
 result = get_multiplied_digits(40203)
 print(result)
+
+# Выполнение задачи с помощью цикла
+def get_multiplied_digits1(number):
+    summ = 1
+    str_number = str(number)
+    for i in str_number:
+        if i == '0':
+            continue
+        else:
+            i = int(i)
+            summ = i * summ
+    return summ
+
+result1 = get_multiplied_digits1(40203)
+print(result1)
