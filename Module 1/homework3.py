778b8b361538f53230feb0c884088e4ad5bc1e42
Aleksey <moon41@yandex.ru>
2024-08-23 19:17:01 +0300

    Специальные методы классов

---
+++ Module 1/homework3.py
@@ -0,0 +1,8 @@
+name = 'Aleksey'
+print('Name:',name)
+age = 34
+print('Age:', age)
+age = age + 5
+print('New Age:', age)
+is_student = True
+print('Is Student:', is_student)
--- homework3.py
+++
@@ -1,8 +0,0 @@
-name = 'Aleksey'
-print('Name:',name)
-age = 34
-print('Age:', age)
-age = age + 5
-print('New Age:', age)
-is_student = True
-print('Is Student:', is_student)
