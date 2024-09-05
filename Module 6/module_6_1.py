93c8f1b3001ccc9f6419326848cd3aa2bcfdc896
Aleksey <moon41@yandex.ru>
2024-09-05 09:23:38 +0300

    Наследование классов

---
+++ Module 6/module_6_1.py
@@ -0,0 +1,59 @@
+class Animal:
+    alive = True  # живой
+    fed = False  # накормленный
+
+    def __init__(self, name):
+        self.name = name  # индивидуальное название каждого животного
+
+
+# class Plant:
+#     edible = False  # съедобность
+#
+#     def __init__(self, name):
+#         self.name = name  # индивидуальное название каждого растения
+
+
+class Mammal(Animal):
+
+    def eat(self, food):
+        if food.edible is True:
+            Animal.fed = True
+            print(f'{self.name} съел {food.name}')
+        else:
+            Animal.alive = False
+            print(f'{self.name} не стал есть {food.name}')
+
+
+class Predator(Animal):
+
+    def eat(self, food):
+        if food.edible is True:
+            Animal.fed = True
+            print(f'{self.name} съел {food.name}')
+        else:
+            Animal.alive = False
+            print(f'{self.name} не стал есть {food.name}')
+
+
+class Flower(Plant):
+    edible = False
+
+
+class Fruit(Plant):
+    edible = True
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
