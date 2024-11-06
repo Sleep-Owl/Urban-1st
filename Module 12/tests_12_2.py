ab6848dac46bc2a2c2746277ec1712bb8f30effa
Aleksey <moon41@yandex.ru>
2024-11-06 11:01:23 +0300

    Методы Юнит-тестирования

---
+++ Module 12/tests_12_2.py
@@ -0,0 +1,49 @@
+import module_12_2
+import unittest
+
+
+class TournamentTest(unittest.TestCase):
+    """
+        Класс для тестирования турниров.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        """
+        Создание объектов бегунов.
+        """
+        self.usain = module_12_2.Runner("Усэйн", 10)
+        self.andrey = module_12_2.Runner("Андрей", 9)
+        self.nick = module_12_2.Runner("Ник", 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for i in cls.all_results.items():
+            r = {}
+            for key, value in i[1].items():
+                r[key] = value.name
+            print(r)
+
+    def test_tournament_usain_nick(self):
+        tournament = module_12_2.Tournament(90, self.usain, self.nick)
+        finishers = tournament.start()
+        last_runner = list(finishers.values())[-1]
+        self.assertTrue(last_runner == self.nick, "Последний бегун должен быть Ник")
+        self.all_results[1] = finishers
+
+    def test_tournament_andrey_nick(self):
+        tournament = module_12_2.Tournament(90, self.andrey, self.nick)
+        finishers = tournament.start()
+        last_runner = list(finishers.values())[-1]
+        self.assertTrue(last_runner == self.nick, "Последний бегун должен быть Ник")
+        self.all_results[2] = finishers
+
+    def test_tournament_usain_andrey_nick(self):
+        tournament = module_12_2.Tournament(90, self.usain, self.andrey, self.nick)
+        finishers = tournament.start()
+        last_runner = list(finishers.values())[-1]
+        self.assertTrue(last_runner == self.nick, "Последний бегун должен быть Ник")
+        self.all_results[3] = finishers
