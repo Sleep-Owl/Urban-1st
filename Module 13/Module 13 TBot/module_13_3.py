91d13edd5642f6f9786a1cccb2951b99d60090c7
Aleksey <moon41@yandex.ru>
2024-11-18 13:25:14 +0300

    Методы отправки сообщений

---
+++ Module 13/Module 13 TBot/module_13_3.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+import Key_for_bot
+
+api = Key_for_bot.api  # используется импортируемый модуль с ключем телеграмм бота
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(text=['/start'])
+async def start(message):
+    await message.answer(f'Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_massages(message):
+    await message.answer(f'Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
