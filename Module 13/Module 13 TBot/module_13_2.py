6751d044a88fab1680c6926247e6608fec4f4997
Aleksey <moon41@yandex.ru>
2024-11-18 12:58:13 +0300

    Хендлеры обработки сообщений

--- Module 13/Module 13 TBot/module_13_2.py
+++ Module 13/Module 13 TBot/module_13_2.py
@@ -1,11 +1,22 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
+import Key_for_bot
 
-api = '7931437044:AAEjDcINV0R8JpEjohF3Tz4IATEcIKQwtU4'
+api = Key_for_bot.api # используется импортируемый модуль с ключем телеграмм бота
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+
+@dp.message_handler(text=['/start'])
+async def start(message):
+    print(f'Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_massages(message):
+    print(f'Введите команду /start, чтобы начать общение.')
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-
