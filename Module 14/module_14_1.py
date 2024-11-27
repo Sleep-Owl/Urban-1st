611c3616fccb41a53c497c8689006a05fb4d32e9
Aleksey <moon41@yandex.ru>
2024-11-27 13:30:51 +0300

    Создание БД, добавление, выбор и удаление элементов

--- Module 14/module_14_1.py
+++ Module 14/module_14_1.py
@@ -15,4 +15,18 @@ balance INTEGER NOT NULL
 
 for i in range(1, 11):
     cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
-                    (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', f'1000'))
+                   (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', f'1000'))
+
+for i in range(1, 11, 2):
+    cursor.execute('UPDATE Users SET balance = ? WHERE username = ?', (500, f'User{i}'))
+
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM Users WHERE id = ? ', (i,))
+
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
+results = cursor.fetchall()
+for result in results:
+    print(result)
+
+connection.commit()
+connection.close()
