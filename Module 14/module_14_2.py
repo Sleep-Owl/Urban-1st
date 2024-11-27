0efdeb1f3218838c7f994bb429a2ace6c2867b45
Aleksey <moon41@yandex.ru>
2024-11-27 13:53:37 +0300

    Выбор элементов и функции в SQL запросах

---
+++ Module 14/module_14_2.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                   (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', f'1000'))
+
+for i in range(1, 11, 2):
+    cursor.execute('UPDATE Users SET balance = ? WHERE username = ?', (500, f'User{i}'))
+
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM Users WHERE id = ? ', (i,))
+
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
+results = cursor.fetchall()
+for result in results:
+    print(result)
+
+cursor.execute('DELETE FROM Users WHERE id = 6')
+
+cursor.execute('SELECT COUNT(*) FROM Users')  # Решение по ДЗ
+total_users = cursor.fetchone()[0]
+
+cursor.execute('SELECT SUM(balance) FROM Users')
+all_balances = cursor.fetchone()[0]
+
+print(all_balances / total_users)
+
+cursor.execute('SELECT AVG(balance) FROM Users')  # Решение задачи с помощью функции AVG
+avg_balance = cursor.fetchone()[0]
+print(avg_balance)
+
+connection.commit()
+connection.close()
